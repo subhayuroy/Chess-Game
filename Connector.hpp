24d4670fccd068e1760637fb8ea73b4c5f6a2a04
Subhayu Roy <55306904+subhayuroy@users.noreply.github.com>
2020-01-06 00:25:29 +0530

    Update Connector.hpp

--- Connector.hpp
+++ Connector.hpp
@@ -31,6 +31,36 @@ void ConnectorToEngine(char* path)
 
     CreateProcess(NULL, path, NULL, NULL, TRUE, 0, NULL, NULL, &sti, &pi);
 }
+std::string getNextMove(std::string position)
+{
+    std::string str;
+    position = "position startpos moves "+position+"\ngo\n";
+    WriterFile(pipin_w, position.c_str(), position.length(), &writ, NULL);
+    Sleep(500);
+
+    PeekNamedPipe(pipout_r, buffer, sizeof(buffer), &read, &available, NULL);
+    do
+        {
+        ZeroMemory(buffer, sizeof(buffer));
+        if(!ReadFile(pipout_r, buffer, sizeof(buffer), &read, NULL) || !read) break;
+        buffer[read] = 0;
+        str += (char*) buffer;
+    }while(read >= sizeof(buffer));
+    int n= str.find("bestmove");
+    if(n!= -1) return str.substr(n+9,4);
+    return  "error";
+}
+
+void CloseConnection()
+{
+    WriterFile(pipin_w, "quit\n", 5, &writ, NULL);
+    if(pipin_w !=NULL) CloseHandle(pipin_w);
+    if(pipin_r != NULL) CloseHandle(pipin_r);
+    if(pipout_w != NULL) CloseHandle(pipout_w);
+    if(pipout_r != NULL) CloseHandle(pipout_r);
+    if(pi.hProcess != NULL) CloseHandle(pi.hProcess);
+    if(pi.hThread != NULL) CloseHandle(pi.hTread);
+}
 
 
 #endif CONNECTOR_H
